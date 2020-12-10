bafb0148924a833d1d450bf84b5881d2d1063264
Mathieu Letrone <61909500+mLetrone@users.noreply.github.com>
2020-12-10 10:04:06 +0100

    add: userRepository

---
+++ Projet/API/src/repositories/userRepository.js
@@ -0,0 +1,72 @@
+const { v4: uuid } = require('uuid');
+const _ = require('lodash');
+const ValidationError = require('./validationError');
+
+const checkUser = function (user) {
+    if (!user.name) {
+        throw new ValidationError('The user must have a name.');
+    }
+    if (!user.age) {
+        throw new ValidationError('The user must have a age.');
+    }
+}
+
+class UserRepository {
+    constructor(db) {
+        this.db = db;
+    }
+
+    getAll() {
+        return this.db.getData("/users");
+    }
+
+    add(user) {
+        checkBook(user);
+        user.id = uuid();
+        user.copies = []; // initialize empty copy array
+        this.db.push("/users[]", user);
+
+        return user;
+    }
+
+    get(id) {
+        const users = this.getAll();
+        return _.find(users, { id });
+    }
+
+    update(id, user) {
+        if (user.id !== id) {
+            throw new ValidationError('You cannot change the identifier.');
+        }
+
+        checkBook(user);
+        const path = this.getIdPath(id);
+        if (path == null) {
+            throw new ValidationError('This user does not exists');
+        }
+
+        this.db.push(path, user);
+
+        return user;
+    }
+
+    delete(id) {
+        const path = this.getIdPath(id);
+        if (path != null) {
+            this.db.delete(path);
+        }
+
+    }
+
+    getIdPath(id) {
+        const users = this.getAll();
+        const index = _.findIndex(users, { id });
+        if (index == -1) {
+            return null;
+        }
+
+        return '/users[' + index + ']';
+    }
+}
+
+module.exports = UserRepository;
