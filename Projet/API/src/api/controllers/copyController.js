bc7d5d31e653f95de849b94c4be1d7ee37e3733a
Mathieu Letrone <61909500+mLetrone@users.noreply.github.com>
2020-12-10 11:21:29 +0100

    fix: methods parameters

--- Projet/API/src/api/controllers/copyController.js
+++ Projet/API/src/api/controllers/copyController.js
@@ -4,18 +4,18 @@ class CopyController {
     }
 
     getAll(req, res) {
-        const copies = this.copyRepository.getAll();
+        const copies = this.copyRepository.getAll(req.params.bookId);
         res.json(copies);
     }
 
     create(req, res) {
-        const copy = this.copyRepository.add(req.body);
+        const copy = this.copyRepository.add(req.body, req.params.bookId);
         res.location('/copies/' + copy.id);
         res.status(201).send(copy);
     }
 
     get(req, res) {
-        const copy = this.copyRepository.get(req.params.copyId);
+        const copy = this.copyRepository.get(req.params.copyId, req.params.bookId);
         if (copy == null) {
             res.status(404).send(null);
         } else {
@@ -24,12 +24,12 @@ class CopyController {
     }
 
     update(req, res) {
-        const copy = this.copyRepository.update(req.params.copyId, req.body)
+        const copy = this.copyRepository.update(req.params.copyId, req.body, req.params.bookId)
         res.status(200).send(copy);
     }
 
     delete(req, res) {
-        this.copyRepository.delete(req.params.copyId);
+        this.copyRepository.delete(req.params.copyId, req.params.bookId);
         res.status(204).send(null);
     }
 }
