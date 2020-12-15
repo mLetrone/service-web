2408aa117e6d86550abd813aa31dd6fc94cecc30
Mathieu Letrone <61909500+mLetrone@users.noreply.github.com>
2020-12-15 15:31:56 +0100

    modif: /!\ not working

--- Projet/Client/src/app/loan-list/loan-list.component.ts
+++ Projet/Client/src/app/loan-list/loan-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, pipe } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+import { Loan } from '../model/Loan';
+import { LoanService } from '../services/loan.service';
+
 
 @Component({
   selector: 'app-loan-list',
@@ -6,10 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loan-list.component.css']
 })
 export class LoanListComponent implements OnInit {
+  public loans$: Observable<Loan[]>;
 
-  constructor() { }
+  constructor(private loanService: LoanService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { this.init();}
+
+  public init() {
+    this.loans$ = this.loanService.getAll()
+          .subscribe();
   }
 
 }
