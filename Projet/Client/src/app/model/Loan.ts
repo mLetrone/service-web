272af3479f94085a06eab846964ee4e5af26f367
Mathieu Letrone <61909500+mLetrone@users.noreply.github.com>
2020-12-15 15:11:35 +0100

    modif: loan model

---
+++ Projet/Client/src/app/model/Loan.ts
@@ -0,0 +1,10 @@
+export class Loan {
+    constructor(
+        public id: string,
+        public bookId: string,
+        public copyId: string,
+        public userId: string,
+        public loanDate: string
+
+    ){}
+}
